d13ac3ec52476fbf2f105f3eeb7c9b846105f394
ak98161615 <ak98161615@gmail.com>
2021-12-26 22:04:15 +0530

    sort01 donw"

--- inverseofNumber.java
+++ inverseofNumber.java
@@ -1,3 +1,20 @@
+import java.util.*;
 public class inverseofNumber {
-    
+   public static void main(String[] args) {
+    Scanner scn=new Scanner(System.in);
+    int n=scn.nextInt();
+   
+  
+    int inverted = 0;
+    int power = 1;
+
+    while (n > 0) {
+      int digit = n % 10;
+      n = n / 10;
+      inverted += power * Math.pow(10, digit - 1);
+      power++;
+    }
+    System.out.println(inverted);
+    scn.close();
+  }
 }
---
+++ sort01.java
@@ -0,0 +1,44 @@
+import java.util.*; 
+public class sort01 {
+      public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    for(int i = 0 ;i < n; i++){
+      arr[i] = scn.nextInt();
+    }
+    sort01s(arr);
+    print(arr);
+  }
+
+
+
+private static void sort01s(int[] arr) {
+    int i=0;
+    int j=0;
+    while(i<arr.length){
+if (arr[i]==0) {
+    swap(arr,i,j);
+    i++;
+    j++;
+}else if(arr[i]==1){
+
+i++;
+
+
+}
+    }
+}
+private static void swap(int[] arr, int i, int j) {
+    System.out.println("Swapping index " + i + " and index " + j);
+    int temp=arr[i];
+    arr[i]=arr[j];
+    arr[j]=temp;
+}
+
+private static void print(int arr[]){
+    for (int x:arr){
+        System.out.println(x);
+    }
+}
+}
