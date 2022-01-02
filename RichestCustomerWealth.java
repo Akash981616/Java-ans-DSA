436ce9e14777a8402fd7fa30224a0af23dfcca57
ak98161615 <ak98161615@gmail.com>
2022-01-02 21:36:58 +0530

    2 stack questions solve

---
+++ RichestCustomerWealth.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class RichestCustomerWealth {
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        int m=scn.nextInt();
+        int arr[][]=new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr[n][m]=scn.nextInt();
+            }
+        }
+        int ans=richest(arr);
+        
+    
+        System.out.println(ans);
+
+
+       
+        
+    }
+
+    private static int richest(int[][] arr) {
+        int max=Integer.MIN_VALUE;
+            for (int row = 0; row < arr.length; row++) {
+            int wealth=0;
+            for (int col = 0; col < arr[row].length; col++) {
+                wealth+=arr[row][col];
+                
+            }
+           if (wealth>max) {
+               max=wealth;
+               
+           }
+        }
+        return max;
+    }
+}
---
+++ findtheNumberWithEvwnDigits.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+
+/**
+ * findtheNumberWithEvwnDigits
+ */
+public class findtheNumberWithEvwnDigits {
+public static void main(String[] args) {
+    Scanner scn=new Scanner(System.in);
+    int n=scn.nextInt();
+    int arr[]=new int[n];
+    for (int i = 0; i < arr.length; i++) {
+        arr[i]=scn.nextInt();
+    }
+    int count=findEvenNumbers(arr);
+    System.out.print(count);
+    
+}
+   public static int  findEvenNumbers(int []arr){
+       int count =0;
+       int digits=0;
+       for(int element:arr){
+             digits=digit(element);
+             if (digits%2==0) {
+                 count++;
+                 
+             }
+       }
+    return count;
+   }
+private static int digit(int element) {
+    int count =0;
+    while (element>0) {
+        element=element/10;
+        count ++;
+        
+        
+    }
+    return count;
+    // second  fast  trick for number of digits
+    // if(element<0){
+    //     element=element*-1;
+    // }
+    // return (int)(Math.log10(element))+1;
+}
+
+
+}
