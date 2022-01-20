6cafd9257696e187b8680a8a403d9eea6ad297fb
ak98161615 <ak98161615@gmail.com>
2022-01-20 22:33:37 +0530

    Kernighans Algo

---
+++ KernighansAlgorithm.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class KernighansAlgorithm {
+
+  public static void main(String[] args){
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int count=0;
+    while(n!=0){
+    int rsb=n&-n;
+
+        n=n-rsb;
+        count++;
+        
+    }
+    System.out.println(count);
+    scn.close();
+}
+
+}
