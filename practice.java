f4c13d825dba93ee6b5bdcfe14a33e61eb25b6f3
ak98161615 <ak98161615@gmail.com>
2022-01-21 21:33:34 +0530

    removeLast LL

---
+++ practice.java
@@ -0,0 +1,15 @@
+public class practice {
+public static void main(String[] args) {
+    int n=13;
+    for (int index = 0; index <n; index++) {
+      int d=n &1;
+      if(d==0){
+          System.out.println("even");
+      }else{
+        System.out.println("odd");
+
+      }
+
+        
+    }
+}}
