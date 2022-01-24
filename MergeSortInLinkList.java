12ce815e46c5a5307bd07e1f9f5aecbd201460e8
ak98161615 <ak98161615@gmail.com>
2022-01-24 23:20:31 +0530

    mergerSort InLinkList

--- MergeSortInLinkList.java
+++ MergeSortInLinkList.java
@@ -1,6 +1,7 @@
 import java.io.*;
 
-public class MidOfLinkList {
+
+public class MergeSortInLinkList {
   public static class Node {
     int data;
     Node next;
@@ -165,7 +166,7 @@ public class MidOfLinkList {
     public void reverseDI() {
       int li = 0;
       int ri = size - 1;
-      while(li < ri){
+      while (li < ri) {
         Node left = getNodeAt(li);
         Node right = getNodeAt(ri);
 
@@ -177,17 +178,17 @@ public class MidOfLinkList {
         ri--;
       }
     }
- 
-    public void reversePI(){
-      if(size <= 1){
+
+    public void reversePI() {
+      if (size <= 1) {
         return;
       }
 
       Node prev = null;
       Node curr = head;
-      while(curr != null){
+      while (curr != null) {
         Node next = curr.next;
-        
+
         curr.next = prev;
         prev = curr;
         curr = next;
@@ -197,91 +198,85 @@ public class MidOfLinkList {
       head = tail;
       tail = temp;
     }
-  
-    public int kthFromLast(int k){
+
+    public int kthFromLast(int k) {
       Node slow = head;
       Node fast = head;
-      for(int i = 0; i < k; i++){
+      for (int i = 0; i < k; i++) {
         fast = fast.next;
       }
 
-      while(fast != tail){
+      while (fast != tail) {
         slow = slow.next;
         fast = fast.next;
       }
 
       return slow.data;
     }
- 
-    public int mid(){
-      Node slow=head;
-      Node fast=head;
-      while (fast!=null&&fast!=null) {
-        fast=fast.next.next;
-        slow=slow.next.next;
 
-        
+    public static Node midNode(Node head,Node tail) {
+      Node f = head;
+      Node s = head;
+
+      while (f.next != null && f.next.next != null) {
+        f = f.next.next;
+        s = s.next;
       }
 
-      return slow.data;
+      return s;
     }
 
+    public static LinkedList mergeTwoSortedLists(LinkedList l1, LinkedList l2) {
+      LinkedList ml = new LinkedList();
+
+      Node one = l1.head;
+      Node two = l2.head;
+      while (one != null && two != null) {
+        if (one.data < two.data) {
+          ml.addLast(one.data);
+          one = one.next;
+        } else {
+          ml.addLast(two.data);
+          two = two.next;
+        }
+      }
+
+      while (one != null) {
+        ml.addLast(one.data);
+        one = one.next;
+      }
+
+      while (two != null) {
+        ml.addLast(two.data);
+        two = two.next;
+      }
+
+      return ml;
+    }
+
+    public static LinkedList mergeSort(Node head, Node tail){
+        Node mid=midNode(head,tail);
+        LinkedList first = mergeSort(head, mid);
+        LinkedList second=mergeSort(mid.next,tail);  
+        LinkedList ans=mergeTwoSortedLists(first,second);   
+        return ans;
+    }
+    
   }
 
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    LinkedList list = new LinkedList();
 
-    String str = br.readLine();
-    while (str.equals("quit") == false) {
-      if (str.startsWith("addLast")) {
-        int val = Integer.parseInt(str.split(" ")[1]);
-        list.addLast(val);
-      } else if (str.startsWith("size")) {
-        System.out.println(list.size());
-      } else if (str.startsWith("display")) {
-        list.display();
-      } else if (str.startsWith("removeFirst")) {
-        list.removeFirst();
-      } else if (str.startsWith("getFirst")) {
-        int val = list.getFirst();
-        if (val != -1) {
-          System.out.println(val);
-        }
-      } else if (str.startsWith("getLast")) {
-        int val = list.getLast();
-        if (val != -1) {
-          System.out.println(val);
-        }
-      } else if (str.startsWith("getAt")) {
-        int idx = Integer.parseInt(str.split(" ")[1]);
-        int val = list.getAt(idx);
-        if (val != -1) {
-          System.out.println(val);
-        }
-      } else if (str.startsWith("addFirst")) {
-        int val = Integer.parseInt(str.split(" ")[1]);
-        list.addFirst(val);
-      } else if (str.startsWith("addAt")) {
-        int idx = Integer.parseInt(str.split(" ")[1]);
-        int val = Integer.parseInt(str.split(" ")[2]);
-        list.addAt(idx, val);
-      } else if (str.startsWith("removeLast")) {
-        list.removeLast();
-      } else if (str.startsWith("removeAt")) {
-        int idx = Integer.parseInt(str.split(" ")[1]);
-        list.removeAt(idx);
-      } else if(str.startsWith("reverseDI")){
-        list.reverseDI();
-      } else if(str.startsWith("reversePI")){
-        list.reversePI();
-      } else if(str.startsWith("kthFromEnd")){
-        int idx = Integer.parseInt(str.split(" ")[1]);
-        System.out.println(list.kthFromLast(idx));
-      } else if(str.startsWith("mid")){
-        System.out.println(list.mid());
-      }
-      str = br.readLine();
+    int n1 = Integer.parseInt(br.readLine());
+    LinkedList l1 = new LinkedList();
+    String[] values1 = br.readLine().split(" ");
+    for (int i = 0; i < n1; i++) {
+      int d = Integer.parseInt(values1[i]);
+      l1.addLast(d);
     }
+
+    LinkedList sorted = LinkedList.mergeSort(l1.head, l1.tail);
+    sorted.display();
+    l1.display();
   }
 }
