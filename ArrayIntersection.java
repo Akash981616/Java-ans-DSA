e9cbaf35720e0b804a3b156caa1ed35d20551cdd
ak98161615 <ak98161615@gmail.com>
2022-01-29 22:59:08 +0530

    intersection of array

---
+++ ArrayIntersection.java
@@ -0,0 +1,52 @@
+import java.util.*;
+public class ArrayIntersection {
+    public static void main(String[] args) {
+        int nums1[]={9,4,9,8,4};
+        int nums2[]={4,9,5};
+        HashMap<Integer, Integer> fmap = new HashMap<>();
+        HashMap<Integer, Integer> rmap = new HashMap<>();
+        for(int val: nums1){
+           if(fmap.containsKey(val)){
+             int of = fmap.get(val);
+             int nf = of + 1;
+             fmap.put(val, nf);
+           } else {
+              fmap.put(val, 1);
+           }
+        }
+    
+        for(int val: nums2){
+           if(fmap.containsKey(val) && fmap.get(val) > 0){
+              int of=fmap.get(val);
+              int nf=of-1;
+              fmap.put(val,nf);
+                rmap.put(val,nf);
+             
+           }
+        }
+    
+     }
+}
+    // }
+    // class Solution {
+    //     public int[] intersection(int[] nums1, int[] nums2) {
+    //         HashSet<Integer> set = new HashSet<Integer>();
+    //         for(int i: nums1){
+    //             set.add(i);
+    //         }
+            
+    //         HashSet<Integer> intersection = new HashSet<Integer>();
+    //         for(int i: nums2){
+    //             if(set.contains(i)){
+    //                 intersection.add(i);
+    //             }
+    //         }
+    //         int[] result = new int[intersection.size()];
+    //         int index = 0;
+    //         for(int i: intersection){
+    //             result[index++] = i;
+    //         }
+            
+    //         return result;
+    //     }
+    // }
--- ValidPalidrome.java
+++ ValidPalidrome.java
@@ -1,19 +1,19 @@
-// https://leetcode.com/problems/valid-palindrome/
-class Solution {
-    public boolean isPalindrome(String s) {
-            String ans=s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
-            int start=0;
-            int end=ans.length()-1;
-            while(start<end){
-                    char c1 = ans.charAt(start);
-                    char c2 = ans.charAt(end);
-                    if(c1!=c2){
-                            return false;
-                    }
-                    start++;
-                    end--;
+// // https://leetcode.com/problems/valid-palindrome/
+// class Solution {
+//     public boolean isPalindrome(String s) {
+//             String ans=s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+//             int start=0;
+//             int end=ans.length()-1;
+//             while(start<end){
+//                     char c1 = ans.charAt(start);
+//                     char c2 = ans.charAt(end);
+//                     if(c1!=c2){
+//                             return false;
+//                     }
+//                     start++;
+//                     end--;
                     
-            }
-        return true;
-    }
-}
+//             }
+//         return true;
+//     }
+// }
