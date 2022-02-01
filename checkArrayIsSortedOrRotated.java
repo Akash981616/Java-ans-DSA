70d228ac6e5fb085dcb956b60faef3a2991e9ed7
ak98161615 <ak98161615@gmail.com>
2022-02-01 20:18:46 +0530

    levelorder genric tree

---
+++ checkArrayIsSortedOrRotated.java
@@ -0,0 +1,11 @@
+public class checkArrayIsSortedOrRotated {
+    class Solution {
+        public boolean check(int[] nums) {
+            int count = 0;
+            for(int i = 1 ; i < nums.length ; i++) if(nums[i-1]>nums[i]) count++;
+            if(nums[nums.length-1]>nums[0]) count++;
+            if(count>1) return false;
+            return true;
+        }
+    }
+}
