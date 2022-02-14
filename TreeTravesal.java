5f89877d47dc72ed5a1405cd3789cc158cccbce9
ak98161615 <ak98161615@gmail.com>
2022-02-14 13:57:23 +0530

    zig zag order traversal

--- TreeTravesal.java
+++ TreeTravesal.java
@@ -126,4 +126,25 @@ class Solution {
             postOrder(res,root.right);
             res.add(root.val);
     }
+    // Zig Zag Traversal
+    class Solution {
+        public List<List<Integer>> levelOrder(TreeNode root) {
+                List<List<Integer>>res=new ArrayList<>();
+                if(root==null)return res;
+                Queue<TreeNode>q=new LinkedList<>();
+                q.add(root);
+                while(q.size()!=0){
+                        List<Integer>curr=new ArrayList<>();
+                        int size=q.size();
+                        for(int i=0;i<size;i++){
+                                TreeNode temp=q.remove();
+                                curr.add(temp.val);
+                            if(temp.left!=null) q.add(temp.left);
+                            if(temp.right!=null) q.add(temp.right);
+                        }
+                        res.add(curr);
+                }
+            return res;
+        }
+    }
 }
