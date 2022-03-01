e044e5095819112f68e4f32344a8c5567e2f8ee3
ak98161615 <ak98161615@gmail.com>
2022-03-01 18:45:53 +0530

    max chunsktomakeArray sorted 2

---
+++ MaxchunskforsortedArray.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxChunksToSorted(int[] arr) {
+        int max=Integer.MIN_VALUE;
+            int count=0;
+            for(int i=0;i<arr.length;i++){
+                    max=Math.max(max,arr[i]);
+                    if(i==max)count++;
+            }
+            return count;
+    }
+}
