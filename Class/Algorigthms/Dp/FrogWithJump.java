5c75e801668e3c5f3950c9874ff2cee24c72b0f7
Akash981616 <Ak98574586@gmail.com>
2022-07-01 00:40:40 +0530

    frog jump

---
+++ Class/Algorigthms/Dp/FrogWithJump.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class FrogWithJump {
+    public boolean canCross(int[] stones) {
+        HashMap<Integer, HashSet<Integer>> map = new HashMap<>();
+        for (int i = 0; i < stones.length; i++) {
+            map.put(stones[i], new HashSet<>());
+        }
+        map.get(stones[0]).add(1);
+        for (int ele : stones) {
+            for (int jumps : map.get(ele)) {
+                int pos = jumps + ele;
+                if (pos == stones[stones.length - 1])
+                    return true;
+                if (jumps != 0 && map.containsKey(pos)) {
+                    map.get(pos).add(jumps - 1);
+                    map.get(pos).add(jumps);
+                    map.get(pos).add(jumps + 1);
+                }
+            }
+        }
+        return false;
+    }
+
+}
+
